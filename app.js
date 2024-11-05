d173ca93b0772806fea158ba2ded2c3d20f1bfc5
VishnuVarshiniP <vishnupvarshini295@gmail.com>
2024-11-05 15:00:06 +0530

    Changes Done

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 8080;
 
-// Set view engine to EJS for dynamic HTML rendering
-app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.send('Hello All, Happy Learning-- Updated By Vishnu Varshini'));
 
-// Set the 'public' folder for static assets (images, CSS, etc.)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Home route - renders a dynamic page with images and text
-app.get('/', (req, res) => {
-    const data = {
-        title: 'Welcome to My App! Happy Learning Modified By Vishnu Varshini',
-        description: 'This is an enhanced version of the app featuring images and dynamic content.',
-        imagePath: '/images/happy-learning.jpg', // Points to an image in the public folder
-        features: [
-            'Interactive Web Pages',
-            'Image and Text Integration',
-            'Templating with EJS',
-            'Serving Static Files',
-        ]
-    };
-    res.render('index', data);
-});
-
-// About route - renders an additional page with more info
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Us',
-        description: 'This app demonstrates Express with EJS templating and static file serving.'
-    });
-});
-
-app.listen(port, () => {
-    console.log(`App running on http://localhost:${port}`);
-});
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
